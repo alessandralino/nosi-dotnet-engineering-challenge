6b02b54dbd2ac68c4083f2882482f6f54de28729
alessandralino <ale.lino.00@gmail.com>
2024-08-08 18:06:04 +0100

    feat: Task 3 - Project configuration to connect with MySQL

--- NOS.Engineering.Challenge.API/Program.cs
+++ NOS.Engineering.Challenge.API/Program.cs
@@ -1,8 +1,19 @@
+using Microsoft.EntityFrameworkCore;
 using NOS.Engineering.Challenge.API.Extensions;
+using NOS.Engineering.Challenge.Database;
 
 var builder = WebApplication.CreateBuilder(args)
         .ConfigureWebHost()
         .RegisterServices();
+string mySqlConnection =
+              builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContextPool<AppDbContext>(options =>
+                options.UseMySql(mySqlConnection,
+                      ServerVersion.AutoDetect(mySqlConnection)));
+
+
+
 var app = builder.Build();
 
 app.MapControllers();
---
+++ NOS.Engineering.Challenge/Database/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NOS.Engineering.Challenge.Models;
+
+namespace NOS.Engineering.Challenge.Database
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Content> Content { get; set; }
+    }
+}
